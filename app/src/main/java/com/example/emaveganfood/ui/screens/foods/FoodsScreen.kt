5d180419e694216624144e0d95393650b2e88abe
Vlad <ricean.vlad7@gmail.com>
2022-11-25 08:55:35 +0200

    -expand item for description

--- app/src/main/java/com/example/emaveganfood/ui/screens/foods/FoodsScreen.kt
+++ app/src/main/java/com/example/emaveganfood/ui/screens/foods/FoodsScreen.kt
@@ -1,22 +1,29 @@
 package com.example.emaveganfood.ui.screens.foods
 
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.unit.dp
 import com.example.emaveganfood.R
 import com.example.emaveganfood.ui.models.Food
@@ -30,20 +37,35 @@ fun FoodsScreen(
     Column {
         LazyColumn {
             items(allFoods) { food ->
-                FoodCard(food = food)
+                FoodItem(food = food)
             }
         }
     }
 }
 
 @Composable
-fun FoodCard(
+fun FoodItem(
     food: Food,
     modifier: Modifier = Modifier
 ) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    val color by animateColorAsState(
+        targetValue = if (expanded) Color.DarkGray else MaterialTheme.colors.surface,
+    )
+
     Card(modifier = Modifier.padding(8.dp), elevation = 4.dp) {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+                .background(color)
         ) {
             Image(
                 painter = painterResource(food.imageResourceId),
@@ -58,11 +80,53 @@ fun FoodCard(
                 modifier = Modifier.padding(8.dp),
                 style = MaterialTheme.typography.h3,
             )
-            Text(
-                text = food.description,
-                modifier = Modifier.padding(16.dp),
-                style = MaterialTheme.typography.h6
+            FoodItemButton(
+                expanded = expanded,
+                onClick = { expanded = !expanded }
             )
+            if(expanded) {
+                FoodDescription(description = food.description)
+            }
         }
+    }
+}
+
+@Composable
+fun FoodItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            tint = MaterialTheme.colors.secondary,
+            contentDescription = stringResource(id = R.string.expand_button_content_description)
+        )
+    }
+}
+
+@Composable
+fun FoodDescription(
+    description: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(
+            start = 16.dp,
+            top = 8.dp,
+            bottom = 16.dp,
+            end = 16.dp
+        )
+    ) {
+        Text(
+            text = stringResource(R.string.about),
+            style = MaterialTheme.typography.h3,
+        )
+        Text(
+            text = description,
+            style = MaterialTheme.typography.body1,
+        )
     }
 }
