5f45a98df1bf40dbf12a67890a79cb58b0eb1ca1
Vlad <ricean.vlad7@gmail.com>
2022-12-02 18:25:17 +0200

    -select image from phone

--- app/src/main/java/com/example/emaveganfood/ui/screens/AddFoodScreen.kt
+++ app/src/main/java/com/example/emaveganfood/ui/screens/AddFoodScreen.kt
@@ -1,24 +1,35 @@
 package com.example.emaveganfood.ui.screens
 
+import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun AddFoodScreen(
     modifier: Modifier = Modifier,
 ) {
+    var hasImage by remember {
+        mutableStateOf(false)
+    }
+
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+
     val imagePicker = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent(),
         onResult = { uri ->
-
+            hasImage = uri != null
+            imageUri = uri
         }
     )
 
@@ -41,6 +52,14 @@ fun AddFoodScreen(
             imagePicker.launch("image/*")
         }) {
             Text(text = "Select Picture")
+        }
+
+        if (hasImage && imageUri != null) {
+            AsyncImage(
+                model = imageUri,
+                modifier = Modifier.fillMaxWidth(),
+                contentDescription = "Selected image",
+            )
         }
 
     }
