ef8d920c0f6c621106ea07baa43356885e7bd3d6
Vlad <ricean.vlad7@gmail.com>
2022-12-02 17:55:38 +0200

    -changed secondary color

--- app/src/main/java/com/example/emaveganfood/ui/screens/FoodsScreen.kt
+++ app/src/main/java/com/example/emaveganfood/ui/screens/FoodsScreen.kt
@@ -5,11 +5,9 @@ import androidx.compose.animation.*
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -18,18 +16,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
@@ -38,7 +28,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.emaveganfood.R
 import com.example.emaveganfood.ui.models.Food
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter", "UnrememberedMutableState")
 @Composable
@@ -100,7 +89,7 @@ fun AddFoodFab(
             onClick = onAddFoodClicked,
             icon = {
                 Icon(Icons.Filled.Add, null)
-            }
+            },
         )
     }
 }
--- app/src/main/java/com/example/emaveganfood/ui/theme/Color.kt
+++ app/src/main/java/com/example/emaveganfood/ui/theme/Color.kt
@@ -4,4 +4,4 @@ import androidx.compose.ui.graphics.Color
 
 val Primary = Color(0xFF633f5c)
 val PrimaryVariant = Color(0xFF916a89)
-val Secondary = Color(0xFFfeffff)
+val Secondary = Color(0xFFd4e6f7)
