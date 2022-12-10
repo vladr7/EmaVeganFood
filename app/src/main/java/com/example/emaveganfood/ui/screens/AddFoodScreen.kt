5c27cf0580937be025b476d5875a0ed21d334af8
Vlad <ricean.vlad7@gmail.com>
2022-12-10 16:46:17 +0200

    -improve design on add food

--- app/src/main/java/com/example/emaveganfood/ui/screens/AddFoodScreen.kt
+++ app/src/main/java/com/example/emaveganfood/ui/screens/AddFoodScreen.kt
@@ -2,6 +2,7 @@ package com.example.emaveganfood.ui.screens
 
 import android.content.Context
 import android.net.Uri
+import android.text.style.BackgroundColorSpan
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -13,6 +14,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -24,6 +26,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.example.emaveganfood.ComposeFileProvider
 import com.example.emaveganfood.R
+import com.example.emaveganfood.ui.theme.PrimaryTransparent
 import com.example.emaveganfood.ui.viewmodels.AddFoodViewModel
 import com.example.emaveganfood.utils.State
 import kotlinx.coroutines.launch
@@ -34,150 +37,157 @@ fun AddFoodScreen(
     modifier: Modifier = Modifier,
     viewModel: AddFoodViewModel = hiltViewModel()
 ) {
-    val coroutineScope = rememberCoroutineScope()
-
-    var hasImage by remember {
-        mutableStateOf(false)
-    }
-
-    var imageUri by remember {
-        mutableStateOf<Uri?>(null)
-    }
-
-    var isLoading by remember {
-        mutableStateOf(false)
-    }
-
-    val imagePicker = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.GetContent(),
-        onResult = { uri ->
-            hasImage = uri != null
-            imageUri = uri
-        }
-    )
-
-    val cameraLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.TakePicture(),
-        onResult = { success ->
-            hasImage = success
-        }
-    )
-
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .fillMaxSize()
+            .background(color = PrimaryTransparent)
     ) {
-        val context = LocalContext.current
+        val coroutineScope = rememberCoroutineScope()
 
-        Spacer(modifier = Modifier.padding(24.dp))
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Button(onClick = {
-                val uri = ComposeFileProvider.getImageUri(context)
+        var hasImage by remember {
+            mutableStateOf(false)
+        }
+
+        var imageUri by remember {
+            mutableStateOf<Uri?>(null)
+        }
+
+        var isLoading by remember {
+            mutableStateOf(false)
+        }
+
+        val imagePicker = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.GetContent(),
+            onResult = { uri ->
+                hasImage = uri != null
                 imageUri = uri
-                cameraLauncher.launch(uri)
-            }) {
-                Text(text = "Take Picture")
             }
-
-            Text("sau", modifier = Modifier.padding(start = 16.dp, end = 16.dp))
-
-            Button(onClick = {
-                imagePicker.launch("image/*")
-            }) {
-                Text(text = "Select Picture")
-            }
-        }
-
-        Spacer(modifier = Modifier.padding(16.dp))
-
-        if (hasImage && imageUri != null) {
-            AsyncImage(
-                model = imageUri,
-                modifier = Modifier
-                    .width(200.dp)
-                    .height(200.dp),
-                contentDescription = "Selected image",
-            )
-        } else {
-            Image(
-                painter = painterResource(id = R.drawable.ic_baseline_no_photography_24),
-                contentDescription = null,
-                modifier = Modifier
-                    .width(200.dp)
-                    .height(200.dp)
-            )
-        }
-
-        Spacer(modifier = Modifier.padding(8.dp))
-
-        OutlinedTextField(
-            value = viewModel.foodTitle,
-            singleLine = true,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 32.dp, end = 32.dp, top = 16.dp),
-            onValueChange = {
-                if (it.length <= 40) {
-                    viewModel.updateFoodTitle(it)
-                }
-            },
-            label = {
-                Text(stringResource(R.string.titlu))
-            },
-            keyboardOptions = KeyboardOptions.Default.copy(
-                imeAction = ImeAction.Next,
-                capitalization = KeyboardCapitalization.Sentences
-            ),
-            keyboardActions = KeyboardActions(
-                onDone = {
-
-                }
-            ),
         )
 
-        OutlinedTextField(
-            value = viewModel.foodDescription,
-            singleLine = false,
+        val cameraLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.TakePicture(),
+            onResult = { success ->
+                hasImage = success
+            }
+        )
+
+        Column(
             modifier = Modifier
+                .fillMaxHeight()
                 .fillMaxWidth()
-                .height(300.dp)
-                .padding(start = 32.dp, end = 32.dp, top = 16.dp),
-            onValueChange = {
-                if (it.length <= 400) {
-                    viewModel.updateFoodDescription(it)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val context = LocalContext.current
+
+            Spacer(modifier = Modifier.padding(24.dp))
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Button(onClick = {
+                    val uri = ComposeFileProvider.getImageUri(context)
+                    imageUri = uri
+                    cameraLauncher.launch(uri)
+                }) {
+                    Text(text = "Camera")
                 }
-            },
-            label = {
-                Text(stringResource(R.string.descriere))
-            },
-            keyboardOptions = KeyboardOptions.Default.copy(
-                imeAction = ImeAction.Done,
-                capitalization = KeyboardCapitalization.Sentences
-            ),
-            keyboardActions = KeyboardActions(
-                onDone = {
-                    if(!isLoading) {
-                        isLoading = true
-                        coroutineScope.launch {
-                            addFood(fileUri = imageUri, viewModel, context, onLoading = {
-                                isLoading = it
-                            })
+
+                Text("sau", modifier = Modifier.padding(top = 8.dp, bottom = 8.dp))
+
+                Button(onClick = {
+                    imagePicker.launch("image/*")
+                }) {
+                    Text(text = "Selecteaza din telefon")
+                }
+            }
+
+            Spacer(modifier = Modifier.padding(16.dp))
+
+            if (hasImage && imageUri != null) {
+                AsyncImage(
+                    model = imageUri,
+                    modifier = Modifier
+                        .width(200.dp)
+                        .height(200.dp),
+                    contentDescription = "Selected image",
+                )
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_baseline_no_photography_24),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(50.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.padding(8.dp))
+
+            OutlinedTextField(
+                value = viewModel.foodTitle,
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 32.dp, end = 32.dp, top = 16.dp),
+                onValueChange = {
+                    if (it.length <= 40) {
+                        viewModel.updateFoodTitle(it)
+                    }
+                },
+                label = {
+                    Text(stringResource(R.string.titlu))
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Next,
+                    capitalization = KeyboardCapitalization.Sentences
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+
+                    }
+                ),
+            )
+
+            OutlinedTextField(
+                value = viewModel.foodDescription,
+                singleLine = false,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .padding(start = 32.dp, end = 32.dp, top = 16.dp),
+                onValueChange = {
+                    if (it.length <= 400) {
+                        viewModel.updateFoodDescription(it)
+                    }
+                },
+                label = {
+                    Text(stringResource(R.string.descriere))
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done,
+                    capitalization = KeyboardCapitalization.Sentences
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        if (!isLoading) {
+                            isLoading = true
+                            coroutineScope.launch {
+                                addFood(fileUri = imageUri, viewModel, context, onLoading = {
+                                    isLoading = it
+                                })
+                            }
                         }
                     }
-                }
-            ),
-        )
+                ),
+            )
 
-        Spacer(modifier = Modifier.padding(32.dp))
+            Spacer(modifier = Modifier.padding(32.dp))
 
+        }
     }
+
 }
 
 private suspend fun addFood(
--- app/src/main/java/com/example/emaveganfood/ui/theme/Color.kt
+++ app/src/main/java/com/example/emaveganfood/ui/theme/Color.kt
@@ -5,3 +5,4 @@ import androidx.compose.ui.graphics.Color
 val Primary = Color(0xFF633f5c)
 val PrimaryVariant = Color(0xFF916a89)
 val Secondary = Color(0xFFd4e6f7)
+val PrimaryTransparent = Color(0x26633f5c)
