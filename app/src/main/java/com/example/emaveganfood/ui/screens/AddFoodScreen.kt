e40ffe9d7677d97aeae447300d99ac8204bcbc41
Vlad <ricean.vlad7@gmail.com>
2022-12-11 10:08:05 +0200

    -moved boilerplate code into another file

--- app/src/main/java/com/example/emaveganfood/ui/screens/AddFoodScreen.kt
+++ app/src/main/java/com/example/emaveganfood/ui/screens/AddFoodScreen.kt
@@ -31,6 +31,8 @@ import com.example.emaveganfood.R
 import com.example.emaveganfood.ui.theme.PrimaryTransparent
 import com.example.emaveganfood.ui.viewmodels.AddFoodViewModel
 import com.example.emaveganfood.utils.State
+import com.example.emaveganfood.utils.getCompressedImage
+import com.example.emaveganfood.utils.getFilePathFromUri
 import id.zelory.compressor.Compressor
 import id.zelory.compressor.constraint.format
 import id.zelory.compressor.constraint.quality
@@ -242,65 +244,4 @@ private suspend fun addFoodImageToStorage(
     }
 }
 
-suspend fun getCompressedImage(imageUri: Uri, context: Context): Uri {
-    val fileUri = getFilePathFromUri(imageUri, context)
-    val compressedImageFile = Compressor.compress(context, File(fileUri?.path.toString())) {
-        quality(50) // combine with compressor constraint
-        size(maxFileSize = 600000)
-        format(Bitmap.CompressFormat.JPEG)
-    }
-    return Uri.fromFile(compressedImageFile)
-}
 
-@Throws(IOException::class)
-fun getFilePathFromUri(uri: Uri, context: Context?): Uri? {
-    val fileName: String? = getFileName(uri, context)
-    val file = File(context?.externalCacheDir, fileName)
-    file.createNewFile()
-    FileOutputStream(file).use { outputStream ->
-        context?.contentResolver?.openInputStream(uri).use { inputStream ->
-            copyFile(inputStream, outputStream)
-            outputStream.flush()
-        }
-    }
-    return Uri.fromFile(file)
-}
-
-@Throws(IOException::class)
-private fun copyFile(`in`: InputStream?, out: OutputStream) {
-    val buffer = ByteArray(1024)
-    var read: Int? = null
-    while (`in`?.read(buffer).also({ read = it!! }) != -1) {
-        read?.let { out.write(buffer, 0, it) }
-    }
-}//copyFile ends
-
-fun getFileName(uri: Uri, context: Context?): String? {
-    var fileName: String? = getFileNameFromCursor(uri, context)
-    if (fileName == null) {
-        val fileExtension: String? = getFileExtension(uri, context)
-        fileName = "temp_file" + if (fileExtension != null) ".$fileExtension" else ""
-    } else if (!fileName.contains(".")) {
-        val fileExtension: String? = getFileExtension(uri, context)
-        fileName = "$fileName.$fileExtension"
-    }
-    return fileName
-}
-
-fun getFileExtension(uri: Uri, context: Context?): String? {
-    val fileType: String? = context?.contentResolver?.getType(uri)
-    return MimeTypeMap.getSingleton().getExtensionFromMimeType(fileType)
-}
-
-fun getFileNameFromCursor(uri: Uri, context: Context?): String? {
-    val fileCursor: Cursor? = context?.contentResolver
-        ?.query(uri, arrayOf<String>(OpenableColumns.DISPLAY_NAME), null, null, null)
-    var fileName: String? = null
-    if (fileCursor != null && fileCursor.moveToFirst()) {
-        val cIndex: Int = fileCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-        if (cIndex != -1) {
-            fileName = fileCursor.getString(cIndex)
-        }
-    }
-    return fileName
-}
