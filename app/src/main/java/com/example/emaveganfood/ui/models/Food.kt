92fff7e6e977a3e40198529f3e642548d4afebdb
Vlad <ricean.vlad7@gmail.com>
2022-12-17 10:31:28 +0200

    -add time in seconds to firestore

--- app/src/main/java/com/example/emaveganfood/ui/models/Food.kt
+++ app/src/main/java/com/example/emaveganfood/ui/models/Food.kt
@@ -1,5 +1,6 @@
 package com.example.emaveganfood.ui.models
 
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentId
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -10,5 +11,6 @@ data class Food(
     @SerialName("id") val id: String = "",
     @SerialName("title") val title: String = "",
     @SerialName("description") val description: String = "",
-    val imageRef: String = ""
+    val imageRef: String = "",
+    val timeInSeconds: Long = Timestamp.now().seconds,
 ): java.io.Serializable
